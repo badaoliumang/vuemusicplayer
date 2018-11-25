0e6b22849a7872951b70edae0110181bc899f8b5
霸道 <1639186757@qq.com>
2018-11-26 00:06:43 +0800

    vue -Router 实现播放器菜单切换

---
+++ src/pages/home.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="">
+    主页面
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,43 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Index from '@/pages/index'
+import Home from "@/pages/home"
+import Artists from "@/pages/artists"
+import ListCate from "@/pages/listcate"
+import Ucenter from "@/pages/ucenter"
+import Search from "@/pages/search"
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+
+      {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Index,
+      children:[
+        {
+          path: 'home',
+          component: Home
+        },
+        {
+            path:"artists",
+            component:Artists
+          },
+          {
+            path:"listcate",
+            component:ListCate
+          },
+          {
+            path:"ucenter",
+            component:Ucenter
+          },
+          {
+            path:"search",
+            component:Search
+          }
+      ]
     }
+
   ]
 })
