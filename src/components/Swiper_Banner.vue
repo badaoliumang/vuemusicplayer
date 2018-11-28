4cf54341b5169e756a7f58aa434100735e9d436d
霸道 <1639186757@qq.com>
2018-11-28 23:46:37 +0800

    实现新歌速递以及swiper组件 实现轮播图切换

---
+++ src/components/Swiper_Banner.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <div class="banner">
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img src="../assets/img/b1.jpg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="../assets/img/b2.jpg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="../assets/img/b3.jpg" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+  data(){
+    return{
+      swiperOption:{
+        pagination: {
+          el: '.swiper-pagination',//swiper的分页器
+        },
+        autoplay:{
+          delay:1000,//间隔一秒滚动一次
+        }//自动滚动
+      }
+    }
+  },
+  components: {
+    swiper,
+    swiperSlide
+  }
+}
+</script>
+
+<style scoped>
+
+.banner{
+  padding: 10px;
+}
+
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,21 @@
 <template lang="html">
   <div class="">
     <TodayRecommend/>
+    <NewsMusic />
+      <SwiperBanner/>
   </div>
 </template>
 
 <script>
 import TodayRecommend from "../components/Today_Recommend"
+import NewsMusic from "../components/News_Music"
+import SwiperBanner from "../components/Swiper_Banner"
 export default {
     name:"home",
     components:{
-    TodayRecommend
+    TodayRecommend,
+    NewsMusic,
+    SwiperBanner
     }
 }
 </script>
